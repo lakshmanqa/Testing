729743da69b7d2f8c24949a3e52dda6e0a58bbce
Lakshman Kumar Vanukuri <lvanukuri@lumafintech.com>
2025-04-25 17:50:38 -0400

    updated

--- tests/playwright.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  use: {
-    headless: true, // Optional: true is recommended for CI
-  },
-  reporter: [['junit', { outputFile: 'test-results/test-results.xml' }]],
-});
